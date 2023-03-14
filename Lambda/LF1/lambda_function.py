0e57206f76f050ae02ee0f3fd7135232b3d08880
Dawei He <hdw623@hotmail.com>
2023-03-14 18:45:15 -0400

    set up LF1 env

---
+++ Lambda/LF1/lambda_function.py
@@ -0,0 +1,82 @@
+import boto3
+from decimal import Decimal
+import json
+import urllib.request
+import urllib.parse
+import urllib.error
+import os
+from requests_aws4auth import AWS4Auth
+
+
+s3 = boto3.client('s3', region_name = 'us-east-1',
+                       aws_access_key_id = os.getenv('KEY_ID'),
+                       aws_secret_access_key = os.getenv('SECRET_KEY'))
+rekognition = boto3.client('rekognition')
+region = 'us-east-1'
+service = 'es'
+credentials = boto3.Session().get_credentials()
+awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
+
+
+
+# --------------- Helper Functions to call Rekognition APIs ------------------
+
+
+def detect_faces(bucket, key):
+    response = rekognition.detect_faces(Image={"S3Object": {"Bucket": bucket, "Name": key}})
+    return response
+
+
+def detect_labels(bucket, key):
+    response = rekognition.detect_labels(Image={"S3Object": {"Bucket": bucket, "Name": key}})
+
+    # Sample code to write response to DynamoDB table 'MyTable' with 'PK' as Primary Key.
+    # Note: role used for executing this Lambda function should have write access to the table.
+    #table = boto3.resource('dynamodb').Table('MyTable')
+    #labels = [{'Confidence': Decimal(str(label_prediction['Confidence'])), 'Name': label_prediction['Name']} for label_prediction in response['Labels']]
+    #table.put_item(Item={'PK': key, 'Labels': labels})
+    return response
+
+
+def index_faces(bucket, key):
+    # Note: Collection has to be created upfront. Use CreateCollection API to create a collecion.
+    #rekognition.create_collection(CollectionId='BLUEPRINT_COLLECTION')
+    response = rekognition.index_faces(Image={"S3Object": {"Bucket": bucket, "Name": key}}, CollectionId="BLUEPRINT_COLLECTION")
+    return response
+
+def is_valid_image_file(key):
+    return key.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif'))
+
+
+# --------------- Main handler ------------------
+
+
+def lambda_handler(event, context):
+    '''Demonstrates S3 trigger that uses
+    Rekognition APIs to detect faces, labels and index faces in S3 Object.
+    '''
+    print("Received event: " + json.dumps(event, indent=2))
+
+    # Get the object from the event
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'])
+    try:
+        # Calls rekognition DetectFaces API to detect faces in S3 object
+        if is_valid_image_file(key):
+            response = detect_faces(bucket, key)
+
+        # Calls rekognition DetectLabels API to detect labels in S3 object
+        #response = detect_labels(bucket, key)
+
+        # Calls rekognition IndexFaces API to detect faces in S3 object and index faces into specified collection
+        #response = index_faces(bucket, key)
+
+        # Print response to console.
+        print(response)
+
+        return response
+    except Exception as e:
+        print(e)
+        print("Error processing object {} from bucket {}. ".format(key, bucket) +
+              "Make sure your object and bucket exist and your bucket is in the same region as this function.")
+        raise e
