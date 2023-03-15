f3d8b44d8b2fa5acfc97c168efa8d70d887f0d70
Dawei He <hdw623@hotmail.com>
2023-03-15 16:51:11 -0400

    finished LF1 and LF2

--- Lambda/LF1/lambda_function.py
+++ Lambda/LF1/lambda_function.py
@@ -1,82 +1,95 @@
 import boto3
-from decimal import Decimal
 import json
 import urllib.request
 import urllib.parse
 import urllib.error
 import os
+import logging
 from requests_aws4auth import AWS4Auth
+from opensearchpy import OpenSearch, RequestsHttpConnection
+from dateutil.tz import tzutc
 
+# logger = logging.getLogger()
+# logger.setLevel(logging.DEBUG)
 
-s3 = boto3.client('s3', region_name = 'us-east-1',
-                       aws_access_key_id = os.getenv('KEY_ID'),
-                       aws_secret_access_key = os.getenv('SECRET_KEY'))
-rekognition = boto3.client('rekognition')
-region = 'us-east-1'
-service = 'es'
-credentials = boto3.Session().get_credentials()
-awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
-
-
-
-# --------------- Helper Functions to call Rekognition APIs ------------------
-
-
-def detect_faces(bucket, key):
-    response = rekognition.detect_faces(Image={"S3Object": {"Bucket": bucket, "Name": key}})
-    return response
-
+# --------------- Helper Functions -----------------------------
 
 def detect_labels(bucket, key):
-    response = rekognition.detect_labels(Image={"S3Object": {"Bucket": bucket, "Name": key}})
-
-    # Sample code to write response to DynamoDB table 'MyTable' with 'PK' as Primary Key.
-    # Note: role used for executing this Lambda function should have write access to the table.
-    #table = boto3.resource('dynamodb').Table('MyTable')
-    #labels = [{'Confidence': Decimal(str(label_prediction['Confidence'])), 'Name': label_prediction['Name']} for label_prediction in response['Labels']]
-    #table.put_item(Item={'PK': key, 'Labels': labels})
-    return response
-
-
-def index_faces(bucket, key):
-    # Note: Collection has to be created upfront. Use CreateCollection API to create a collecion.
-    #rekognition.create_collection(CollectionId='BLUEPRINT_COLLECTION')
-    response = rekognition.index_faces(Image={"S3Object": {"Bucket": bucket, "Name": key}}, CollectionId="BLUEPRINT_COLLECTION")
-    return response
-
-def is_valid_image_file(key):
-    return key.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif'))
-
-
-# --------------- Main handler ------------------
-
-
-def lambda_handler(event, context):
-    '''Demonstrates S3 trigger that uses
-    Rekognition APIs to detect faces, labels and index faces in S3 Object.
-    '''
-    print("Received event: " + json.dumps(event, indent=2))
-
-    # Get the object from the event
-    bucket = event['Records'][0]['s3']['bucket']['name']
-    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'])
+    rekognition = boto3.client('rekognition')
     try:
-        # Calls rekognition DetectFaces API to detect faces in S3 object
-        if is_valid_image_file(key):
-            response = detect_faces(bucket, key)
-
-        # Calls rekognition DetectLabels API to detect labels in S3 object
-        #response = detect_labels(bucket, key)
-
-        # Calls rekognition IndexFaces API to detect faces in S3 object and index faces into specified collection
-        #response = index_faces(bucket, key)
-
-        # Print response to console.
-        print(response)
-
-        return response
+        response = rekognition.detect_labels(Image={
+            "S3Object": {
+                "Bucket": bucket,
+                "Name": key}})
     except Exception as e:
         print(e)
         print("Error processing object {} from bucket {}. ".format(key, bucket) +
               "Make sure your object and bucket exist and your bucket is in the same region as this function.")
         raise e
+    labels = [label_prediction['Name'].lower() for label_prediction in response['Labels']]
+
+    return labels
+
+
+
+def is_valid_image_file(key):
+    return key.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif'))
+
+
+def get_labels_and_created_time(bucket, key):
+    s3 = boto3.client('s3', region_name='us-east-1',
+                      aws_access_key_id=os.getenv('KEY_ID'),
+                      aws_secret_access_key=os.getenv('SECRET_KEY'))
+    detected_lables = detect_labels(bucket, key)
+    image_metadata = s3.head_object(Bucket=bucket, Key=key)
+    # logger.debug(image_metadata)
+    print(image_metadata)
+    if ('x-amz-meta-customlabels' in image_metadata['ResponseMetadata']['HTTPHeaders'] and
+            image_metadata['ResponseMetadata']['HTTPHeaders']['x-amz-meta-customlabels']):
+        custom_lables = image_metadata['ResponseMetadata']['HTTPHeaders']['x-amz-meta-customlabels'].split(',')
+    else:
+        custom_lables = []
+    created_time = image_metadata['LastModified']
+    labels = list(set(detected_lables + custom_lables))
+    print(labels)
+    return labels, created_time
+
+
+def build_a1(bucket, created_time, key, labels):
+    a1 = {
+        'objectKey': key,
+        'bucket': bucket,
+        'createdTimestamp': created_time.strftime("%Y-%m-%dT%H:%M:%S"),
+        'labels': labels
+    }
+    a1 = json.dumps(a1)
+    return a1
+
+def open_search_create_index(a1):
+    region = 'us-east-1'
+    service = 'es'
+    credentials = boto3.Session().get_credentials()
+    awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
+    open_search = OpenSearch(
+        hosts=[{'host': os.getenv('OS_HOST'), 'port': 443}],
+        http_auth=awsauth,
+        use_ssl=True,
+        verify_certs=True,
+        connection_class=RequestsHttpConnection
+    )
+    response = open_search.index(index='photos', body=a1)
+    return response
+
+# --------------- Main handler ------------------
+
+
+def lambda_handler(event, context):
+    for record in event['Records']:
+        bucket = record['s3']['bucket']['name']
+        key = urllib.parse.unquote_plus(record['s3']['object']['key'])
+        labels, created_time = get_labels_and_created_time(bucket, key)
+        print(labels)
+        a1 = build_a1(bucket, created_time, key, labels)
+        response = open_search_create_index(a1)
+        print(response)
+
